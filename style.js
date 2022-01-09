39aa85fac01d4a5a8d463a13bddc84f705ec8d79
Wojtek Skrobot <97315300+skrobot-wojtek@users.noreply.github.com>
2022-01-09 22:39:55 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,19 @@
+function zegar(){
+    let czas = new Date();
+
+    let godzina = czas.getHours();
+    if (godzina<10){godzina='0'+godzina};
+    let minuta = czas.getMinutes();
+    if (minuta<10){minuta='0'+minuta};
+    let sekunda = czas.getSeconds();
+    if (sekunda<10){sekunda='0'+sekunda};
+    let dzien = czas.getDate();
+    if (dzien<10){dzien='0'+dzien};
+    let miesiac = czas.getMonth()+1;
+    if (miesiac<10){miesiac='0'+miesiac}
+    let rok = czas.getFullYear();
+    
+    document.getElementById('clock').innerHTML = godzina+'-'+minuta+'-'+sekunda+' ------ '+dzien+'-'+miesiac+'-'+rok;;
+    
+    setTimeout(zegar, 1000);
+} 
